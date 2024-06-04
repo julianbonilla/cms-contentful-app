49dccf28ecb1fc32503795b106620dd9dae3f9e4
Julian Bonilla <julianbonilla@users.noreply.github.com>
2024-06-04 12:28:19 -0400

    Update main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter } from "next/font/google";
 import { EXAMPLE_PATH, CMS_NAME } from "@/lib/constants";
 
 export const metadata = {
-  title: `Next.js and ${CMS_NAME} Example`,
-  description: `This is a blog built with Next.js and ${CMS_NAME}.`,
+  title: `Amplify + Next.js + ${CMS_NAME}`,
+  description: `This is a blog built with Amplify, Next.js, and ${CMS_NAME}.`,
 };
 
 const inter = Inter({
